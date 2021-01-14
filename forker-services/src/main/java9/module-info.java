bdc11340cc5c439e0cb9438547ed805765eeab61
brett-smith <brett@nervepoint.com>
2021-01-14 16:53:39 +0000

    Use proper module name now.

--- forker-services/src/main/java9/module-info.java
+++ forker-services/src/main/java9/module-info.java
@@ -6,7 +6,7 @@ module com.sshtools.forker.services {
 	requires transitive com.sshtools.forker.client;
 	requires java.logging;
 	requires transitive java.desktop;
-	requires transitive dbus.java;
+	requires transitive org.freedesktop.dbus;
 	requires transitive java.systemd;
 	exports com.sshtools.forker.services;
 
