82b430c5281ac7ab7a1a21f11afdae2e117a50d0
brett-smith <brett@nervepoint.com>
2021-02-15 23:21:42 +0000

    Build fix

--- forker-updater-swt/src/main/java11/module-info.java
+++ forker-updater-swt/src/main/java11/module-info.java
@@ -2,12 +2,7 @@ module com.sshtools.forker.updater.swt {
 	requires java.desktop;
 	requires java.prefs;
 	requires transitive org.eclipse.swt;
-	/* TODO need to sort this out somehow. Either have mulitple 
-	 * module-infos, one for each OS or include all in the classpath
-	 * and make them static
-	 */
-	/* requires static transitive org.eclipse.swt.gtk.linux.x86_64; */
-	requires transitive org.eclipse.swt.win32.win32.x86_64;
+//	requires transitive org.eclipse.swt.${osgi.platform}; 
 	requires transitive com.sshtools.forker.updater;
 	requires transitive com.sshtools.forker.wrapper;
     exports com.sshtools.forker.updater.swt;
