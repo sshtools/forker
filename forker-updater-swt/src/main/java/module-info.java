2360f24d17e4e6456830a3348908d788d422493c
Brett Smith <brett@logonbox.com>
2021-04-18 16:00:11 +0100

    Jenkins is linux so we must use this for now until a better solution is found. SWT (OSGi?) does not play well with JPMS

--- forker-updater-swt/src/main/java/module-info.java
+++ forker-updater-swt/src/main/java/module-info.java
@@ -2,8 +2,13 @@ module com.sshtools.forker.updater.swt {
 	requires java.desktop;
 	requires java.prefs;
 	requires transitive org.eclipse.swt;
-//	requires transitive org.eclipse.swt.${osgi.platform}; 
-	requires static org.eclipse.swt.win32.win32.x86_64;
+	
+	/* Jenkins is linux so we must use this for now until a better solution
+	 * is found. SWT (OSGi?) does not play well with JPMS
+	 */
+	requires transitive org.eclipse.swt.gtk.linux.x86_64;
+//	requires static org.eclipse.swt.win32.win32.x86_64;
+	
 	requires transitive com.sshtools.forker.updater;
 	requires transitive com.sshtools.forker.wrapper;
     exports com.sshtools.forker.updater.swt;
